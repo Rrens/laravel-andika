5d515c0c07b04ecb7ac0fc6a50c0c97a2ba0abc6
Andika <andikana884@gmail.com.com>
2026-06-06 20:20:57 +0700

    ubah ke controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
@@ -11,5 +12,45 @@ class UserController extends Controller
         $data = User::all();
 
         return view('add', compact('data'));
+    }
+
+    public function store(Request $request)
+    {
+        User::create([
+            'name' => $request->input('nama-panjang'),
+            'email' => $request->input('email'),
+            'password' => $request->input('password'),
+        ]);
+
+        return back();
+    }
+
+    public function edit($id)
+    {
+        $data = User::find($id);
+
+        return view('update', compact('data'));
+    }
+
+    public function update(Request $request)
+    {
+        $user = User::find($request->input('id'));
+
+        $user->update([
+            'name' => $request->input('nama-panjang'),
+            'email' => $request->input('email'),
+            'password' => $request->input('password'),
+        ]);
+
+        return redirect('/login');
+    }
+
+    public function destroy($id)
+    {
+        $user = User::find($id);
+
+        $user->delete();
+
+        return back();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,67 +1,18 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/login', function() {
-//     $data = User::all();
+Route::get('/login', [UserController::class, 'index'])->name('login');
 
-//     return view('add', compact('data'));
-// });
+Route::post('/buat-login', [UserController::class, 'store'])->name('buat-login');
 
-Route::get('add-user', [UserController::class, 'index'])->name('add-user');
+Route::get('/update/{id}', [UserController::class, 'edit'])->name('update');
 
-Route::post('buat-login', function(){
-    // dd(request()->all());
+Route::post('/buat-update', [UserController::class, 'update'])->name('buat-update');
 
-    User::create([
-        'name' => request('nama-panjang'),
-        'email' => request('email'),
-        'password' => request('password'),
-    ]);
-
-    return back();
-})->name('buat-login');
-
-Route::get('/update/{id}', function($id) {
-    $data = User::find($id);
-    // dd($data);
-    return view('update', compact('data'));
-})->name('update');
-
-Route::post('buat-update', function(){
-    $id = request('id');
-    $user = User::find($id);
-    // dd($user, request()->all());
-
-    $user->update([
-        'name' => request('nama-panjang'),
-        'email' => request('email'),
-        'password' => request('password'),
-    ]);
-
-    return redirect('/login');
-})->name('buat-update');
-
-Route::get('/delete/{id}', function($id) {
-    $user = User::find($id);
-    $user->delete();
-
-    return back();
-})->name('delete');
+Route::get('/delete/{id}', [UserController::class, 'destroy'])->name('delete');
